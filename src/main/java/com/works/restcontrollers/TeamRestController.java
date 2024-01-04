b77f20522c745a16734d8c7dd038b5d1b9a16420
Alper SEVİNÇLER <alper.sevincler@gmail.com>
2024-01-04 04:02:14 +0300

    Update TeamRestController.java

--- src/main/java/com/works/restcontrollers/TeamRestController.java
+++ src/main/java/com/works/restcontrollers/TeamRestController.java
@@ -1,9 +1,11 @@
 package com.works.restcontrollers;
 
 import com.works.services.TeamService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class TeamRestController {
 
     final TeamService teamService;
+    final HttpServletRequest req;
+
+    @PostMapping("/teamInsert/{teamName}")
+    public ResponseEntity teamInsert(@PathVariable String teamName) {
+        boolean loginStatus = req.getSession().getAttribute("user") == null;
+
+        //loginStatus null(boş) ise giriş yapılmamış anlamına gelir ve geriye "Please Login" cevabı yollanır
+        if (loginStatus) {
+            return new ResponseEntity("Please Login", HttpStatus.UNAUTHORIZED);
+        }
+
+        return teamService.save(teamName);
+    }
+
 
 }
