96c942e0b756389fd84ad1e444c7d6ffa35d38b3
Alper SEVİNÇLER <alper.sevincler@gmail.com>
2024-01-05 01:25:26 +0300

    Update FootballerRepository.java

--- src/main/java/com/works/repositories/FootballerRepository.java
+++ src/main/java/com/works/repositories/FootballerRepository.java
@@ -2,6 +2,7 @@ package com.works.repositories;
 
 import com.works.entities.Footballer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -10,6 +11,9 @@ public interface FootballerRepository extends JpaRepository<Footballer, Long> {
     Optional<Footballer> findByEmailEqualsIgnoreCase(String email);
 
     Optional<Footballer> findByEmailEqualsIgnoreCaseAndPasswordEquals(String email, String password);
+
+    @Query(value = "select F.NAME, F.SURNAME, F.AGE from TEAM inner join FOOTBALLER F on TEAM.FID = F.FID where TEAM.NAME = ?1 order by F.AGE asc limit ?2 offset ?3", nativeQuery = true)
+    Object getTeamCreate();
 
 
 }
